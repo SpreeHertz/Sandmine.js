f190478c7bab09d436131759fd92c8459c93bf4c
SpreeHertz <spreehertzofficial@gmail.com>
2021-10-31 19:42:47 +0530

    improve onKick

--- src/events/onKick.js
+++ src/events/onKick.js
@@ -2,5 +2,5 @@ const bot = require('../../index');
 const chalk = require('chalk');
 
 bot.on('kicked', () => {
-	console.log(chalk.yellowBright('warn') + chalk.yellow(` ${process.env.bot_username} got kicked/banned from the server.`));
+	console.log(chalk.yellowBright('warn') + chalk.yellow(` ${process.env.bot_username} got kicked/banned from the server, or the server was stopped.`));
 });
