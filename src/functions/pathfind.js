7d968447bed9960b6637a3a81823f7b74f28b72e
SpreeHertz <spreehertzofficial@gmail.com>
2021-10-31 20:00:10 +0530

    Update pathfind.js

--- src/functions/pathfind.js
+++ src/functions/pathfind.js
@@ -2,28 +2,33 @@
 
 const bot = require('../../index');
 const { Movements, pathfinder, goals } = require('mineflayer-pathfinder');
-const playerIGN = require('../../config.json').playerToLook;
 const GoalFollow = goals.GoalFollow;
 
 bot.loadPlugin(pathfinder);
 
-
-const followPlayer = () => {
-	const playerToFollow = bot.players[playerIGN];
-
-	if (!playerToFollow || !playerToFollow.entity) {
-		bot.chat(`I can't see you ${playerIGN}`);
-		return;
-	}
-	const mcData = require('minecraft-data')(bot.version);
-	const movements = new Movements(bot, mcData);
-	bot.pathfinder.setMovements(movements);
-
-	const goal = new GoalFollow(playerToFollow.entity, 1);
-	bot.pathfinder.setGoal(goal, true);
-};
-
-
 bot.on('spawn', () => {
-	followPlayer();
+	bot.on('chat', (username, message) => {
+		if (username === bot.username) return;
+		if (message === 'follow me' || message === 'come' || message === 'follow me bot' || message === 'come with me' || message === 'come w/ me') {
+			bot.chat('Sure, following you.');
+		}
+
+
+		const target = bot.players[username]?.entity;
+		if (!target || !target.entity) {
+			bot.chat(`I can't see you ${target}!`);
+			return;
+		}
+
+		const mcData = require('minecraft-data')(bot.version);
+		const movements = new Movements(bot, mcData);
+		bot.pathfinder.setMovements(movements);
+
+		const goal = new GoalFollow(target.entity, 1);
+		bot.pathfinder.setGoal(goal, true);
+		if (message === 'stop following me' || message === 'stop following' || message === 'stop') {
+			bot.chat('Stopped following you.');
+		}
+	});
+
 });
